126291482be4d34a14848d7c1fba6f58855a9cab
zhenghe <zhenghe@madadata.com>
2016-06-27 15:57:19 +0800

    add tests and refactor package.json

--- test/MDButtonWithSpinner.spec.js
+++ test/MDButtonWithSpinner.spec.js
@@ -5,15 +5,54 @@ import expectJSX from 'expect-jsx'
 expect.extend(expectJSX);
 import MDButtonWithSpinner from 'src/MDButtonWithSpinner';
 import MDButton from 'mdbutton';
+import Spinner from 'react-spinkit';
 
 describe('MDButtonWithSpinner', () => {
-  it('should render MDButton', () => {
+
+  it('should render MDButton if showSpinner equals false', () => {
     const renderer = createRenderer();
-    renderer.render(<MDButtonWithSpinner />);
+    renderer.render(
+      <MDButtonWithSpinner
+        showSpinner={false}
+      />
+    );
     const actualElement = renderer.getRenderOutput();
     const expectedElement = <MDButton>{' '}</MDButton>;
     expect(actualElement).toIncludeJSX(expectedElement);
   });
+
+  it('should render pulse Spinner if showSpiner equals true', () => {
+    const renderer = createRenderer();
+    renderer.render(
+      <MDButtonWithSpinner
+        showSpinner
+      />
+    );
+    const actualElement = renderer.getRenderOutput();
+    const expectedElement = <Spinner spinnerName="pulse"/>;
+    expect(actualElement.props.spinnerName).toEqual('pulse');
+    expect(actualElement).toIncludeJSX(expectedElement);
+  })
+
+  it('should render children', () => {
+    const renderer = createRenderer();
+    renderer.render(
+      <MDButtonWithSpinner
+        showSpinner={false}
+      >
+        <div>Hello world</div>
+      </MDButtonWithSpinner>
+    );
+    const actualElement = renderer.getRenderOutput();
+    expect(actualElement.props.children[1].type).toEqual('div');
+  })
+
+  it('should not props onClick down if there is no specified onClick method', () => {
+    const renderer = createRenderer();
+    renderer.render(<MDButtonWithSpinner />);
+    const actualElement = renderer.getRenderOutput();
+    expect(actualElement.props.onClick).toNotExist();
+  })
 
   it('should call onClick when clicked', () => {
     const renderder = createRenderer();
