a6a457a13fc965061aad3b3a4f08d900d8dcd38d
zhenghe <zhenghe@madadata.com>
2016-06-27 10:51:23 +0800

    add onClick test

--- src/__tests__/MDButtonWithSpinner.spec.js
+++ src/__tests__/MDButtonWithSpinner.spec.js
@@ -7,11 +7,20 @@ import MDButtonWithSpinner from '../MDButtonWithSpinner';
 import MDButton from 'mdbutton';
 
 describe('MDButtonWithSpinner', () => {
-  it('should works', () => {
+  it('should render MDButton', () => {
     const renderer = createRenderer();
     renderer.render(<MDButtonWithSpinner />);
     const actualElement = renderer.getRenderOutput();
     const expectedElement = <MDButton>{' '}</MDButton>;
     expect(actualElement).toIncludeJSX(expectedElement);
   });
+
+  it('should call onClick when clicked', () => {
+    const renderder = createRenderer();
+    let hasClicked = false;
+    let onClick = () => hasClicked = true;
+    renderder.render(<MDButtonWithSpinner onClick={onClick} />);
+    renderder.getRenderOutput().props.onClick();
+    expect(hasClicked).toBe(true);
+  })
 });
